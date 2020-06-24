586639f9ac2db831955e1191d6c536721a90f689
Mosè Giordano <mose@gnu.org>
2020-06-24 17:53:00 +0100

    Add logging capability

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,9 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+// Logging
+var nammuOutputChannel = vscode.window.createOutputChannel("Nammu");
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -14,10 +17,13 @@ export function activate(context: vscode.ExtensionContext) {
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('ucl-rsdg.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+	    // The code you place here will be executed every time your command is executed
 
-		// Display a message box to the user
-	    vscode.window.showWarningMessage('Hello VS Code from ucl-rsdg!');
+	    // Display a message box to the user
+	    var str = 'Hello VS Code from ucl-rsdg!';
+	    vscode.window.showWarningMessage(str);
+	    nammuOutputChannel.show();
+	    nammuOutputChannel.append(str + '\n');
 	});
 
 	context.subscriptions.push(disposable);

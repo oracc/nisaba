497acb96e13d9710e3c1b7002c9548a78f05759a
Anastasis Georgoulas <a.georgoulas@ucl.ac.uk>
2021-02-15 09:26:50 +0000

    Create MIME message rather than text

---
+++ src-val/trymime.ts
@@ -0,0 +1,69 @@
+const mimemessage = require('mimemessage');
+
+function createEnvelope() {
+    let envelope = mimemessage.factory({
+        contentType: 'application/xop+xml; charset="utf-8"; type="application/soap+xml"',
+        contentTransferEncoding: 'binary'
+    })
+    envelope.body = `<?xml version="1.0" encoding="UTF-8"?>
+               <SOAP-ENV:Envelope
+                   xmlns:SOAP-ENV="http://www.w3.org/2003/05/soap-envelope"
+                   xmlns:SOAP-ENC="http://www.w3.org/2003/05/soap-encoding"
+                   xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                   xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+                   xmlns:xop="http://www.w3.org/2004/08/xop/include"
+                   xmlns:xmime5="http://www.w3.org/2005/05/xmlmime"
+                   xmlns:osc-data="http://oracc.org/wsdl/ows.xsd"
+                   xmlns:osc-meth="http://oracc.org/wsdl/ows.wsdl">
+                   <SOAP-ENV:Body>
+                       <osc-meth:Request>
+                           <osc-data:keys>
+                               <osc-data:key>atf</osc-data:key>
+                               <osc-data:key>cams/gkab</osc-data:key>
+                               <osc-data:key>00atf/belsunu.atf</osc-data:key>
+                           </osc-data:keys>
+                           <osc-data:data>
+                               <osc-data:item xmime5:contentType="*/*">
+                                   <xop:Include href="cid:request_zip"/>
+                               </osc-data:item>
+                           </osc-data:data>
+                       </osc-meth:Request>
+                   </SOAP-ENV:Body>
+               </SOAP-ENV:Envelope>`;
+    envelope.header('MIME-Version', '1.0');
+    envelope.header('Content-ID', '<SOAP-ENV:Envelope>');
+    return envelope;
+}
+
+function createAttachment() {
+    let attachment = mimemessage.factory({
+        contentType: '*/*',
+        contentTransferEncoding: 'binary'
+    });
+    attachment.header('MIME-Version', '1.0');
+    attachment.header('Content-ID', '<request_zip>');
+    attachment.body = 'adfasdfasdfsadfdaf encoded text goes here';
+    return attachment;
+}
+
+function createMultipart() {
+    let multipartOptions = {
+        charset: 'utf-8',
+        type: 'application/xop+xml',
+        start: '<SOAP-ENV:Envelope>',
+        'start-info': 'application/soap+xml',
+        boundary: '==========boundary========'
+    }
+
+    let message = mimemessage.factory({
+        contentType: 'multipart/related; charset="utf-8"; type="application/xop+xml"; start="<SOAP-ENV:Envelope>"; start-info="application/soap+xml"; boundary="==========boundary========"',
+        body: []
+    })
+    message.header('MIME-Version', '1.0');
+    message.body.push(createEnvelope());
+    message.body.push(createAttachment());
+    return message;
+}
+
+let message = createMultipart();
+console.log(message.toString({noHeaders: true}));

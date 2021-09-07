05554301caa46d343f14c05082af37382e8ce864
ageorgou <1186102+ageorgou@users.noreply.github.com>
2021-09-07 12:12:54 +0100

    Get project code from ATF (#70)
    * Test project extraction on two files
    
    Not implemented yet!
    
    * Get code naively
    
    * Use native EOL
    
    * Start handling multiple codes per file
    
    In case this occurs, we'll get an error but at least be notified.
    
    * Fail when no/multiple project codes are found
    
    * Test for expected errors getting project code

---
+++ src/atf_model.ts
@@ -0,0 +1,22 @@
+import { EOL } from 'os';
+
+/**
+ * Extract the project code from an ATF file.
+ *
+ * @param atfText The contents of the ATF file
+ * @returns The code of the Oracc project the file refers to
+ * @throws An error if there is not exactly 1 #project tag in the file
+ */
+export function getProjectCode(atfText: string): string {
+    const tag = "#project:";
+    const codes = atfText.split(EOL)
+                    .filter(line => line.startsWith(tag))
+                    .map(line => line.slice(tag.length).trim());
+    if (codes.length == 1) {
+        return codes[0];
+    }
+    if (codes.length == 0) {
+        throw 'Project code not found in ATF';
+    }
+    throw 'Multiple project codes found in ATF';
+}
---
+++ src/test/suite/atf.test.ts
@@ -0,0 +1,30 @@
+import * as assert from 'assert';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as vscode from 'vscode';
+import { getProjectCode } from '../../atf_model';
+
+suite('ATF Model Test Suite', () => {
+    vscode.window.showInformationMessage('Start ATF tests.');
+
+    [["belsunu.atf", "cams/gkab"], ["AD6_164.atf", "adsd/adart6"]]
+        .forEach(function ([fileName, projectCode]) {
+            test(`Extract project code correctly for ${fileName}`, async() => {
+                const text = fs.readFileSync(
+                    path.join(__dirname,
+                              `../../../src/test/suite/input/${fileName}`)).toString();
+                assert.strictEqual(getProjectCode(text), projectCode);
+            });
+        });
+
+    [["two_projects.atf", 2], ["no_project.atf", 0]]
+        .forEach(function ([fileName, numCodes]) {
+            test(`Getting project fails when ${numCodes} codes present`, async() => {
+                const text = fs.readFileSync(
+                    path.join(__dirname,
+                              `../../../src/test/suite/input/${fileName}`)).toString();
+                assert.throws(() => getProjectCode(text));
+            });
+        });
+
+});

ea29b10c08331e78da39d6cceadd255d6acef23d
Mosè Giordano <mose@gnu.org>
2022-05-13 19:33:42 +0100

    Add tests for lemmatisation

---
+++ src/test/suite/lemmatisation.test.ts
@@ -0,0 +1,46 @@
+import * as assert from 'assert';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as vscode from 'vscode';
+import { lemmatise } from '../../server/messages';
+
+
+suite('Lemmatisation Test Suite', () => {
+    vscode.window.showInformationMessage('Start lemmatisation tests.');
+
+    for (const [file, project] of [['english', 'ztcc'], ['arabic', 'cams/gkab']]) {
+        test(`Lemmatisation results for ${file}_no_lem.atf`, async () => {
+            const text = fs.readFileSync(
+                path.join(__dirname,
+                          `../../../src/test/suite/input/${file}_no_lem.atf`)).toString();
+            const server_result = await lemmatise(`${file}_no_lem.atf`, project, text);
+            const lemmatised_text = fs.readFileSync(
+                path.join(__dirname,
+                          `../../../src/test/suite/reference/${file}_with_lem.atf`)).toString();
+
+            assert(!server_result.contains_errors());
+            assert(server_result.atf_content === lemmatised_text);
+        });
+    }
+
+    for (const [file, project] of [['english', 'ztcc'], ['arabic', 'cams/gkab']]) {
+        test(`Lemmatisation results for ${file}_with_lem.atf`, async () => {
+            const text = fs.readFileSync(
+                path.join(__dirname,
+                          `../../../src/test/suite/reference/${file}_with_lem.atf`)).toString();
+            const server_result = await lemmatise(`${file}_with_lem.atf`, project, text);
+
+            assert(!server_result.contains_errors());
+            assert(text === server_result.atf_content);
+        });
+    }
+
+    test('Lemmatisation results when there are errors', async () => {
+        const text = fs.readFileSync(
+            path.join(__dirname,
+                      '../../../src/test/suite/input/english_broken.atf')).toString();
+        const server_result = await lemmatise('english_broken.atf', 'ztcc', text);
+
+        assert(server_result.contains_errors());
+    });
+});
